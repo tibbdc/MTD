8b21bd7dd807846a678e4016c74ffee481af129f
dongchen <1219131962@qq.com>
2024-03-18 16:32:44 +0800

    main

--- omic_analysis/enrichment_analysis/kegg_enrich.R
+++ omic_analysis/enrichment_analysis/kegg_enrich.R
@@ -12,7 +12,7 @@ library(dplyr)
 option_list <- list(
   make_option(c("-w", "--workdir"), type = "character", default = NULL, help = "工作目录路径", metavar = "workdir"),
   make_option(c("-s", "--species"), type = "character", default = "Myceliophthora thermophila", help = "菌种名称", metavar = "species"),
-  make_option(c("-p", "--p_adjust"), type = "numeric", default = 0.05, help = "pvalue阈值", metavar = "pvalue"),
+  make_option(c("-p", "--padjust"), type = "numeric", default = 0.05, help = "pvalue阈值", metavar = "pvalue"),
   make_option(c("-i", "--input"), type = "character", default = NULL, help = "输入文件路径", metavar = "file"),
   make_option(c("-o", "--output"), type = "character", default = NULL, help = "输出文件路径", metavar = "file")
 )
@@ -58,7 +58,7 @@ enrich_kegg <- enricher(genelist,
   TERM2GENE = ko2gene,
   TERM2NAME = ko2name,
   pAdjustMethod = "BH", # 使用FDR进行校正
-  pvalueCutoff = opt$p_adjust,
+  pvalueCutoff = opt$padjust,
   qvalueCutoff = 1
 )
 
